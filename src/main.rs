542d762887e55374f7f47c106d155365021f4563
goutham <gouthamreddy09@gmail.com>
2023-01-15 14:09:52 -0500

    push 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Read, io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +8,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+                stream.write("+PONG/r/n".as_bytes()).unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
